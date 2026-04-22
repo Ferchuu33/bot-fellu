2b2078fbd20388833605599ff29c6bea0e08c74d
Ferchuu33 <fer2011movil@gmail.com>
2026-04-22 20:43:00 +0200

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests, telebot, time
 from datetime import datetime, timedelta
 import pytz
 
-TOKEN = "PON_AQUI_TU_TOKEN_DEL_BOTFATHER"
-CHAT_ID = "PON_AQUI_TU_NUMERO_DEL_USERINFOBOT" 
+TOKEN = "8601022037:AAGSAklgevPh0_y44ETbgfQTcqP7B-aGwXo"
+CHAT_ID = "6358542941" 
 TAG_TUYO = "G9RGC9QG"      
 TAG_FELLU = "RYLLQRUVU"  
 
@@ -19,11 +19,11 @@ def sacar_copas(tag):
     return data['trophies'], data['name']
 
 def mandar_reporte():
-    copas_yo, nombre_yo = sacar_copas(TAG_TUYO)
+    copas_Fer, nombre_Fer = sacar_copas(TAG_TUYO)
     copas_fellu, nombre_fellu = sacar_copas(TAG_FELLU)
     
     if not copas_yo or not copas_fellu:
-        bot.send_message(CHAT_ID, "Error: No pude sacar las copas. Revisa si los tags están bien.")
+        bot.send_message(CHAT_ID, "Error ❌: No he podido sacar las copas. Revisa si los tags están bien.")
         return
 
     dif = copas_yo - copas_fellu
@@ -33,13 +33,12 @@ def mandar_reporte():
     msg = f"**{datetime.now(zona_españa).strftime('%d %b')}**\n"
     msg += f"{nombre_yo}: {copas_yo} | {nombre_fellu}: {copas_fellu}\nDiferencia: {dif}\n"
     if dif < 0:
-        msg += f"Le pillas el {fecha_pillas.strftime('%d %b')} a +150/día"
+        msg += f" Ánimo Fer! Le pillas el {fecha_pillas.strftime('%d %b')} a +150/día"
     else:
-        msg += "Ya le ganaste 🥳"
-    
+        msg += "Ya le has ganado, campeón 💪🏽😊🪄"    
     bot.send_message(CHAT_ID, msg, parse_mode='Markdown')
 
-bot.send_message(CHAT_ID, "Bot encendido. Te aviso cada día a las 20:00 hora España")
+bot.send_message(CHAT_ID, "Bot encendido, Fer 🔥. Te aviso cada día a las 20:00 hora España")
 
 ultimo_envio = None
 while True:
